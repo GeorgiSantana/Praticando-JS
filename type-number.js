0d440da9612f55608d05bc9810f492046c20ed41
Georgi Santana <georgi.andersson@hotmail.com>
2023-06-21 10:36:54 -0300

    Praticando type-number

---
+++ type-number.js
@@ -0,0 +1,24 @@
+// tipo Number
+
+const meuNumero = 3;
+
+const primeiroNumero = 5;
+const segundoNumero = 2;
+
+const operacaoMatematica = primeiroNumero * segundoNumero;
+
+console.log (operacaoMatematica)
+
+// ponto flutuante
+
+const nunPontoFlut = 3.3
+const nunPontoFlutSemZero = .5;
+
+const novaOperacao = primeiroNumero / nunPontoFlut;
+
+console.log(novaOperacao)
+
+// NaN -> Not A Number (não é um numero)
+
+const matematica = "Letra"
+console.log (matematica * primeiroNumero)
