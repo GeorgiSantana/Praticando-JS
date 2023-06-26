b86d52093b0f84f5e8e0ab377b72e2de19fa2999
Georgi Santana <georgi.andersson@hotmail.com>
2023-06-25 23:48:05 -0300

    Praticando type-string

---
+++ type-string.js
@@ -0,0 +1,10 @@
+const texto1 =  "Olá, mundo!";
+const texto2 = "Olá, mundo!";
+const senha = "senhaSuperSegura4561!";
+const StringDeNumeros = "123456";
+
+const citacao = 'Meu nome é ';
+const meuNome = "Georgi"
+console.log(citacao + meuNome)
+
+// A concatenação não funciona com outros operadores, apenas com o operador de adição.
