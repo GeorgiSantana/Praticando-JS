49bb9f47c8277201a6c0c6216cf7d860a3aaf767
Georgi Santana <georgi.andersson@hotmail.com>
2023-06-26 00:04:45 -0300

    Praticando type-boolean

---
+++ type-boolean.js
@@ -0,0 +1,12 @@
+const primeiroNumero = 10;
+const segundoNumero = 10;
+// boolean
+// true-> verdadeiro
+// false-> falso
+
+//console.log(primeiroNumero===segundoNumero);
+
+const texto1 = "Alua";
+const texto2 = "Alura";
+
+console.log(texto1==texto2);
