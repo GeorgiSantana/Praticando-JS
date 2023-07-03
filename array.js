fdf52ffbc9877aab36216d6329defe7bb42f34de
Georgi Santana <georgi.andersson@hotmail.com>
2023-07-02 21:40:14 -0300

    Exemplificando Array

---
+++ array.js
@@ -0,0 +1,13 @@
+// Array
+const animals = [
+  "lion",
+  "Monkey",
+  //object pode ser um elemento de um Arrey
+  {
+    name: "Georgi",
+    age: 36,
+  },
+];
+
+//Acessar valorerds dentro do Array
+console.log(animals[2].name);
